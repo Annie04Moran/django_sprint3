400664bbc3c12ff561bb12e1bab98f163a440e5d
Anna <an-na1997@yandex.ru>
2023-05-13 00:50:27 +0300

    jioj

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -24,12 +24,11 @@ def index(request):
 def post_detail(request, id):
     template = 'blog/detail.html'
     post_list = get_object_or_404(Post.objects.select_related(
-        'location', 'author', 'category'
-                                                             ).filter(
+        'location', 'author', 'category').filter(
             pub_date__lte=timezone.now(),
             is_published=True,
             category__is_published=True
-                                                                     ),
+        ),
         pk=id
     )
     context = {'post': post_list}
@@ -40,12 +39,11 @@ def category_posts(request, category_slug):
     template = 'blog/category.html'
     category = get_object_or_404(Category,
                                  slug=category_slug,
-                                 is_published=True
-                                 )
+                                 is_published=True)
     post_list = Post.objects.filter(
         pub_date__lte=timezone.now(),
         is_published=True,
         category=category
-                                   )
+    )
     context = {'category': category, 'post_list': post_list}
     return render(request, template, context)
